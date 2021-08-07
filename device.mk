e13c40af105e76d740ce526fa3b60c3dcc011069
Alexander Koskovich <zvnexus@outlook.com>
2021-08-07 12:33:33 -0700

    lmi: Add permissions for Hotword Enrollment.
     * This is already covered by GMS but in order to account
       for non-GMS builds we should place some duplicates here.
    
    Change-Id: I2d0f504106cd4784669a60b514c027fb6974c816

--- device.mk
+++ device.mk
@@ -342,6 +342,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
+PRODUCT_PACKAGES += \
+    hiddenapi-package-allowlist-product.xml \
+    privapp-permissions-product.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
