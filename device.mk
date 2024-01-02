98b82f638db5fcf21a7bbc58e509ec7f054b7277
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-02 10:29:49 -0300

    lmi: overlay: Add wifi overlay for mainline modules
    Change-Id: Ia0bc27ffe6ba2934729d2e59c614b17b0d549eef

--- device.mk
+++ device.mk
@@ -300,7 +300,8 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     FrameworksResLmi \
     SystemUIResLmi \
-    WifiResTarget
+    WifiResTarget \
+    WifiMainline
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
